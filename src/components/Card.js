2c8a4328c166b9b61b0ba481a08f90ac5cefbe50
John Warren <johnv8@hotmail.co.uk>
2022-07-12 09:38:10 +0100

    improve card layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,13 +6,37 @@ function Card(props) {
             <h1 className="check__title">{props.title}</h1>
             <form className="form__full" >
                 <label for="from">From:</label>
-                <input type="text" id="from" name="from" placeholder="Manchester"/>
+                <input 
+                type="text" 
+                id="from" 
+                name="from" 
+                placeholder="Manchester"
+                style={{width: "150px"}}
+                />
                 <label for="to">To:</label>
-                <input type="text" id="to" name="to" placeholder="London"/>
-                <input type="checkbox" id="oneWay" name="oneWay" value="true" />
-                <label for="oneWay">One Way</label>            
-                <label for="nTrips">Trips in Month:</label>
-                <input type="number" id="nTrips" name="nTrips" placeholder="20"/>  
+                <input 
+                type="text" 
+                id="to" 
+                name="to" 
+                placeholder="London"
+                style={{width: "150px"}}
+                />
+                <label for="nTrips">Trips a<br/>month:</label>
+                <input 
+                type="number" 
+                id="nTrips" 
+                name="nTrips" 
+                placeholder={props.exTrips}
+                style={{width: "50px"}}
+                />  
+                <label for="oneWay">One<br/>Way</label>            
+                <input 
+                type="checkbox" 
+                id="oneWay" 
+                name="oneWay" 
+                value="true"
+                style={{width: "25px"}} 
+                />
             </form>
       </div>
     );
--- src/components/FullCheck.js
+++ src/components/FullCheck.js
@@ -3,9 +3,10 @@ import Card from "./Card";
 function FullCheck() {
     return (
       <div className="full__check">
-        <Card title="Work"/>
-        <Card title="Shopping"/>
-        <Card title="Other"/>
+        <Card title="Work" exTrips="20"/>
+        <Card title="Shopping" exTrips="3"/>
+        <Card title="School" exTrips="10"/>
+        <Card title="Other" exTrips="1"/>
       </div>
     );
   }
